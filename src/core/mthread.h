76f19c0a44a462816a66c19d794539a61bfb67ac
Akvelog <sceneviper@hotmail.com>
2018-12-17 00:48:01 +0900

    Initial upload.

--- src/core/mthread.h
+++ src/core/mthread.h
@@ -3,23 +3,7 @@
 #include    <common/stdc_common.h>
 #include    <common/linux_common.h>
 #include    <common/list.h>
-#include    <core/scheduler.h>
 
+struct a6_mthread;
 struct a6_mthread_pool;
 
-struct a6_mthread {
-    intrusive;
-    pthread_t ptid;
-    struct a6_scheduler *sched;
-    struct a6_mthread_pool *pool;
-    struct {
-        void *(*func)(void *);
-        void *arg;
-    } entrance;
-};
-
-struct a6_mthread_pool {
-    uint32_t size;
-    struct link_index mthq;
-    struct a6_mthread mths[];
-};
