c1d4b20232eebcdf2454c360db60de1236c452bc
Akvelog <sceneviper@hotmail.com>
2018-12-17 01:13:05 +0900

    Initial upload.

--- src/core/mthread.c
+++ src/core/mthread.c
@@ -2,21 +2,44 @@
 #include    <core/scheduler.h>
 #include    <core/iomonitor.h>
 
-
 struct a6_mthread {
     intrusive;
     pthread_t ptid;
     struct a6_scheduler sched;
     struct a6_iomonitor *iomon;
     struct a6_mthread_pool *pool;
-    struct {
-        void *(*func)(void *);
-        void *arg;
-    } entrance;
 };
 
 struct a6_mthread_pool {
+    uint64_t max_n_uth;
     uint32_t size;
     struct link_index mthq;
     struct a6_mthread mths[];
 };
+
+static 
+void *mthread_entrance(void *arg) {
+    struct a6_mthread *self = arg;
+    schedloop(&(self->sched));
+    return NULL;
+}
+
+#define     DEFAULT_IO_CAP          1024
+#define     DEFAULT_MAX_N_UTH       8192
+
+struct a6_mthread *a6_mthread_init(struct a6_mthread *mth) {
+    mth->iomon = a6_iomonitor_create(DEFAULT_IO_CAP);
+    a6_scheduler_init(&(mth->sched), DEFAULT_MAX_N_UTH, mth->iomon);
+    return mth;
+}
+
+struct a6_mthread *a6_mthread_ruin(struct a6_mthread *mth) {
+    return 
+        a6_iomonitor_destroy(mth->iomon),
+        a6_scheduler_ruin(&(mth->sched)),
+        mth;
+}
+
+int a6_mthread_launch(struct a6_mthread *mth) {
+    return pthread_create(&(mth->ptid), NULL, mthread_entrance, mth);
+}
--- src/core/scheduler.h
+++ src/core/scheduler.h
@@ -41,3 +41,5 @@ struct a6_asynck {
 };
 
 void schedloop(struct a6_scheduler *s);
+struct a6_scheduler *a6_scheduler_init(struct a6_scheduler *sched, uint64_t max_n_uth, struct a6_iomonitor *iomon);
+struct a6_scheduler *a6_scheduler_ruin(struct a6_scheduler *sched);
