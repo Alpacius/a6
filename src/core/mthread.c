a74c3e4163a7d184228d41af1dcf4fbade7256a4
Akvelog <sceneviper@hotmail.com>
2018-12-17 09:34:08 +0900

    Initial upload.

--- src/core/mthread.c
+++ src/core/mthread.c
@@ -11,7 +11,6 @@ struct a6_mthread {
 };
 
 struct a6_mthread_pool {
-    uint64_t max_n_uth;
     uint32_t size;
     struct link_index mthq;
     pthread_barrier_t init_barrier;
@@ -29,10 +28,10 @@ void *mthread_entrance(void *arg) {
 #define     DEFAULT_IO_CAP          1024
 #define     DEFAULT_MAX_N_UTH       8192
 
-struct a6_mthread *a6_mthread_init(struct a6_mthread *mth, struct a6_mthread_pool *pool) {
+int a6_mthread_init(struct a6_mthread *mth, struct a6_mthread_pool *pool) {
     mth->iomon = a6_iomonitor_create(DEFAULT_IO_CAP);
     a6_scheduler_init(&(mth->sched), DEFAULT_MAX_N_UTH, mth->iomon);
-    return (mth->pool = pool), mth;
+    return (mth->pool = pool), 1;
 }
 
 struct a6_mthread *a6_mthread_ruin(struct a6_mthread *mth) {
@@ -42,6 +41,29 @@ struct a6_mthread *a6_mthread_ruin(struct a6_mthread *mth) {
         mth;
 }
 
-int a6_mthread_launch(struct a6_mthread *mth, struct a6_mthread_pool *pool) {
+int a6_mthread_launch(struct a6_mthread *mth) {
     return pthread_create(&(mth->ptid), NULL, mthread_entrance, mth);
+}
+
+int a6_mthread_pool_init(struct a6_mthread_pool *pool) {
+    list_init(&(pool->mthq));
+    pthread_barrier_init(&(pool->init_barrier), NULL, pool->size);
+    for (uint32_t i = 0; i < pool->size; i++) {
+        list_add_tail(intrusion_from_ptr(&(pool->mths[i])), &(pool->mthq));
+        a6_mthread_init(&(pool->mths[i]), pool);
+    }
+    return 1;
+}
+
+void a6_mthread_pool_ruin(struct a6_mthread_pool *pool) {
+    list_init(&(pool->mthq));
+    for (uint32_t i = 0; i < pool->size; i++)
+        a6_mthread_ruin(&(pool->mths[i]));
+    pthread_barrier_destroy(&(pool->init_barrier));
+}
+
+int a6_mthread_pool_launch(struct a6_mthread_pool *pool) {
+    for (uint32_t i = 0; i < pool->size; i++)
+        a6_mthread_launch(&(pool->mths[i]));
+    return 1;
 }
