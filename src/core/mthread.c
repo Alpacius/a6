2c7d20bc1024ccc10c1a9395b5832d1cad22f8c2
Akvelog <sceneviper@hotmail.com>
2018-12-17 22:57:10 +0900

    Initial upload.

--- src/core/mthread.c
+++ src/core/mthread.c
@@ -28,12 +28,14 @@ void *mthread_entrance(void *arg) {
 #define     DEFAULT_IO_CAP          1024
 #define     DEFAULT_MAX_N_UTH       8192
 
+static
 int a6_mthread_init(struct a6_mthread *mth, struct a6_mthread_pool *pool) {
     mth->iomon = a6_iomonitor_create(DEFAULT_IO_CAP);
     a6_scheduler_init(&(mth->sched), DEFAULT_MAX_N_UTH, mth->iomon);
     return (mth->pool = pool), 1;
 }
 
+static
 struct a6_mthread *a6_mthread_ruin(struct a6_mthread *mth) {
     return 
         a6_iomonitor_destroy(mth->iomon),
@@ -41,12 +43,14 @@ struct a6_mthread *a6_mthread_ruin(struct a6_mthread *mth) {
         mth;
 }
 
+static
 int a6_mthread_launch(struct a6_mthread *mth) {
     return pthread_create(&(mth->ptid), NULL, mthread_entrance, mth);
 }
 
-int a6_mthread_pool_init(struct a6_mthread_pool *pool) {
-    list_init(&(pool->mthq));
+static
+int a6_mthread_pool_init(struct a6_mthread_pool *pool, uint32_t size) {
+    list_init(&(pool->mthq)), (pool->size = size);
     pthread_barrier_init(&(pool->init_barrier), NULL, pool->size);
     for (uint32_t i = 0; i < pool->size; i++) {
         list_add_tail(intrusion_from_ptr(&(pool->mths[i])), &(pool->mthq));
@@ -55,15 +59,26 @@ int a6_mthread_pool_init(struct a6_mthread_pool *pool) {
     return 1;
 }
 
-void a6_mthread_pool_ruin(struct a6_mthread_pool *pool) {
+static
+struct a6_mthread_pool *a6_mthread_pool_ruin(struct a6_mthread_pool *pool) {
     list_init(&(pool->mthq));
     for (uint32_t i = 0; i < pool->size; i++)
         a6_mthread_ruin(&(pool->mths[i]));
     pthread_barrier_destroy(&(pool->init_barrier));
+    return pool;
 }
 
 int a6_mthread_pool_launch(struct a6_mthread_pool *pool) {
     for (uint32_t i = 0; i < pool->size; i++)
         a6_mthread_launch(&(pool->mths[i]));
     return 1;
+}
+
+struct a6_mthread_pool *a6_mthread_pool_create(uint32_t size) {
+    struct a6_mthread_pool *pool = malloc(sizeof(struct a6_mthread_pool) + size * sizeof(struct a6_mthread));
+    return likely(pool != NULL) ? (a6_mthread_pool_init(pool, size), pool) : NULL;
+}
+
+void a6_mthread_pool_destroy(struct a6_mthread_pool *pool) {
+    free(a6_mthread_pool_ruin(pool));
 }
--- src/core/mthread.h
+++ src/core/mthread.h
@@ -7,3 +7,6 @@
 struct a6_mthread;
 struct a6_mthread_pool;
 
+void a6_mthread_pool_destroy(struct a6_mthread_pool *pool);
+struct a6_mthread_pool *a6_mthread_pool_create(uint32_t size);
+int a6_mthread_pool_launch(struct a6_mthread_pool *pool);
