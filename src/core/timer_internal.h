419c2e7c7ba91d6418f294bb897f2eec45ba8f07
Akvelog <sceneviper@hotmail.com>
2019-01-07 09:36:53 +0900

    Timer support.

--- src/core/timer_internal.h
+++ src/core/timer_internal.h
@@ -27,7 +27,7 @@ struct a6_timer a6i_s_timer_diff(int tval) {
 
 #define a6i_s_timer_compare(t1_, t2_) \
     ({ \
-        struct a6_timer *t1__ = (t1_), *t2__ = (t2_); \
-        uint64_t tv1__ = t1__->alarm.val.epoch, tv2__ = t2->alarm.val.epoch; \
+        const struct a6_timer *t1__ = (t1_), *t2__ = (t2_); \
+        uint64_t tv1__ = t1__->alarm.val.epoch, tv2__ = t2__->alarm.val.epoch; \
         (tv1__ > tv2__) ? 1 : ( (tv1__ < tv2__) ? -1 : 0 ); \
     })
--- src/core/timer_queue.c
+++ src/core/timer_queue.c
@@ -1,1 +1,7 @@
 #include    <core/timer_queue.h>
+
+
+static
+int timer_compare(const void *lhs, const void *rhs) {
+    return a6i_s_timer_compare(lhs, rhs);
+}
